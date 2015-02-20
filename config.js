07e98b425c75ce05f46cb318f37f7868fcaa5077
pratik patel <pratik@mypatelspace.com>
2015-02-20 01:35:16 -0500

    adding missing packages to package.json

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'vM8Fc5aUvvOpqTYsqGXSofCyG',
+    consumer_secret: 'cBhvMKvtgDowJgIqTTy6hXts5R14fBb8imvvcgPSLAf4vrdFgj',
+    access_token_key: '19755623-ZUE1E4GblTsecQQcR7bwzEbiqSBflsiPXTRF447d8',
+    access_token_secret: 'mWtkt1jNsdDFnzuz1wlCX1rTMkoWS3aDj1cYCSdIq63iS'
+  }
+}
